5e98d89735e9c817b7e7853c8d8d75bb581b72d0
楊畯棋(Jayson Yang) <65242479+Jayson-Chichi@users.noreply.github.com>
2026-09-19 19:20:05 +0800

    Open Google Maps location before navigation

--- app.public.js
+++ app.public.js
@@ -1,4 +1,4 @@
-import { initDashboard } from "./app-core.js?v=20260919-public";
+import { initDashboard } from "./app-core.js?v=20260919-location";
 import { placeIdeas } from "./data/places.ideas.js?v=20260919-public";
 
 initDashboard({ placeIdeas });
