d534006369521d3c9ebc0d0cb7d733368435eaa9
Jayson Yang <65242479+Jayson-Chichi@users.noreply.github.com>
2026-09-22 09:38:12 +0800

    Refresh public entry cache for hotel update

--- app.public.js
+++ app.public.js
@@ -1,4 +1,4 @@
-import { initDashboard } from "./app-core.js?v=20260922-daily-plans";
+import { initDashboard } from "./app-core.js?v=20260922-riva-hotel";
 import { placeIdeas } from "./data/places.ideas.js?v=20260922-daily-plans";
 
 initDashboard({ placeIdeas });
